ab945881d44d8279d70b61c9e550cccc87848cbc
lerneri <leticiarneri@alu.ufc.br>
2022-09-04 11:45:37 -0300

    Começada a tarefa 1

---
+++ Tarefa 1/tarefa_1.m
@@ -0,0 +1,13 @@
+
+clear all;
+z = 1+sqrt(-1)
+real (z)
+imag(z)
+figure(1)
+clf
+plot (z, 'ro')
+xlabel('componente real');
+ylabel('componente imaginária');
+title('Gerar um número complexo no plano');
+z2=1+10i
+hold on
