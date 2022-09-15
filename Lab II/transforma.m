cc667531b657ef5b4fbda68309aad0e9fac3e1a7
MiraiiF <112816417+MiraiiF@users.noreply.github.com>
2022-09-15 19:09:27 -0300

    Add files via upload

---
+++ Lab II/transforma.m
@@ -0,0 +1,4 @@
+function y=transforma(a,b) #declarando função com os parâmetros exigidos
+  n=linspace(0,4*pi,200) #declarando vetor
+  y=cos((a*n)+b) #função arbitrária escolhida pra representar a função "x" do roteiro
+endfunction #fim da função
